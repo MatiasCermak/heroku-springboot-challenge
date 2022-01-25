74daf18c2b36c4cb15385d21d4239470fb6c7c52
MatiasCermak <matias.cermak99@gmail.com>
2022-01-25 17:04:23 -0300

    Initial commit

---
+++ src/main/java/com/mcrmk/springboot/nginx1/controller/NginxController.java
@@ -0,0 +1,19 @@
+package com.mcrmk.springboot.nginx1.controller;
+
+import com.mcrmk.springboot.nginx1.model.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("")
+public class NginxController {
+
+    @GetMapping("")
+    public List<Message> testNginx(){
+        return List.of(new Message("Hola Mundo"), new Message("Server 1"));
+    }
+
+}
